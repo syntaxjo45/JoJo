6b79171a471828c316b05d367c3feb981a6271f0
syntaxjo45 <jonathansianturi48@gmail.com>
2021-02-10 11:21:37 +0700

    Vave

---
+++ index.php
@@ -0,0 +1,290 @@
+<!-- re-Code by Jo -->
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    
+    <title>VAVE GLOBAL</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/logo.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/logo.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logo.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo.png">
+    <link rel="manifest" href="assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileImage" content="assets/img/logo.png">
+    <meta name="theme-color" content="#ffffff">
+    <!-- footer src -->
+    <style>
+    .footer {
+      position: relative;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      background-color: #dedee5;
+      color: white;
+      text-align: center;
+    } 
+    
+    .cashback-center{
+        display: flex;
+        justify-content: center;
+    }
+    .container{
+			width: 960px;
+			margin: 0 auto;
+		}
+		button#back2top{
+		    position: fixed;
+		    right: 2%;
+		    bottom: 2%;
+		    opacity: 0;
+		    visibility: hidden;
+		    
+		    /*For better looking button*/
+		    font-family: sans-serif;
+		    font-size: 5px;
+		    line-height: 1.3;
+        border-radius: 50%;
+		    border: none;
+		    outline: none;
+		    cursor: pointer;
+		    padding: 10px 18px;
+		    margin: 0;
+		}
+    </style>
+    <!-- end of footer src -->
+
+    <!-- Library Google -->
+    <link href="assets/css/theme.css" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/443e00f667.js" crossorigin="anonymous"></script>
+
+  </head>
+    
+
+  <body>
+    <main class="main" id="top">
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" data-navbar-on-scroll="data-navbar-on-scroll">
+        <div class="container"><a class="navbar-brand" href="#"><img class="img-fluid" src="https://www.vave.co.id/wp-content/uploads/2020/10/cropped-V-icon-192x192.png" alt="" width="30px"/></a>
+          <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto ms-lg-4 ms-xl-7 border-bottom border-lg-bottom-0 pt-2 pt-lg-0">
+              <li class="nav-item"><a class="nav-link fw-bold active" aria-current="page" href="#">Home</a></li>
+              <li class="nav-item"><a class="nav-link fw-medium" href="about-us/">Tentang Kami</a></li>
+              <li class="nav-item"><a class="nav-link fw-medium" href="layanan/">Layanan Kami</a></li>
+              <li class="nav-item"><a class="nav-link fw-medium" href="help/">Pusat Bantuan</a></li>
+            </ul>
+            <form style="text-align: center;"><a class="btn btn-light rounded-pill shadow fw-bold" href="#" role="button"> <li class="fa fa-button"></li><i class="fas fa-trophy"></i> Top Mentor Titanium
+              <svg class="bi bi-arrow-right" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#79bb86" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+              </svg></a></form>
+          </div>
+        </div>
+      </nav>
+
+
+      
+      <section class="py-5">
+
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-5 col-lg-7 order-md-1 pt-8"><img class="img-fluid" src="assets/img/illustrations/hero-header.png" alt="" /></div>
+            <div class="col-md-7 col-lg-5 text-center text-md-start pt-5 pt-md-9">
+              <h3 class="mb-4 display-2 fw-bold">VAVE <br class="d-block d-lg-none d-xl-block" />#BERBAGI<br/>#CASHBACK</h3>
+              <a class="btn btn-lg btn-info rounded-pill" href="about-us/" role="button">Pelajari lebih lanjut</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="py-6">
+        <div class="container-lg">
+          <div class="row flex-center mb-5">
+            <div class="col-auto text-center my-4">
+              <h1 class="mb-4 fw-bold">Kenapa harus kami ?</h1>
+              <p style="font-size: 2vh;">Bersama, Kita Membangun Bisnis Berbagi Ekonomi</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 mb-5">
+              <div class="card px-5 px-md-3 py-lg-5">
+                <div class="row flex-center">
+                  <div class="bg-holder z-index-1 d-none d-lg-block" style="background-image:url(assets/img/illustrations/feature-bg-1.png);background-position:center;background-size:contain;">
+                  </div>
+
+                  <div class="bg-holder z-index-1 d-block d-lg-none" style="background-image:url(assets/img/illustrations/feature-bg-1.png);background-position:center;background-size:cover;">
+                  </div>
+
+                  <div class="col-md-4 pe-0 pe-md-0 text-center"><img class="img-fluid" src="assets/img/illustrations/feature-search.png" alt="" /></div>
+                  <div class="col-md-8 ps-md-3 pe-md-2 text-center text-md-start z-index-2">
+                    <div class="card-body px-0">
+                      <h4 class="card-title pt-md-5">Inovasi</h4>
+                      <p class="mb-0" style="font-size: 1em;"> <br class="d-none d-lg-block">Aplikasi Belanja Dengan Program <br class="d-none d-lg-block">Belanja Yang Lengkap Dari Berbagai<br class="d-none d-lg-block"> Kategori Usaha Secara<br class="d-none d-lg-block"> Online Maupun Offline<br class="d-none d-lg-block">Satu Aplikasi Untuk Semua Transaksi</p>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 mb-5">
+              <div class="card px-5 px-md-3 py-lg-5">
+                <div class="row flex-center">
+                  <div class="bg-holder z-index-1 d-none d-lg-block" style="background-image:url(assets/img/illustrations/feature-bg-1.png);background-position:center;background-size:contain;">
+                  </div>
+
+                  <div class="bg-holder z-index-1 d-block d-lg-none" style="background-image:url(assets/img/illustrations/feature-bg-1.png);background-position:center;background-size:cover;">
+                  </div>
+
+                  <div class="col-md-4 pe-0 pe-md-0 text-center"><img class="img-fluid" src="assets/img/illustrations/feature-hour.png" alt="" /></div>
+                  <div class="col-md-8 ps-md-3 pe-md-2 text-center text-md-start z-index-2">
+                    <div class="card-body px-0">
+                      <h4 class="card-title pt-md-5">Komunitas </h4>
+                      <p class="mb-0" style="font-size: 1em;"> Aplikasi Belanja Yang Memiliki <br class="d-none d-lg-block">Sistem Membangun Komunitas <br class="d-none d-lg-block">Belanja Antara Member Dan <br class="d-none d-lg-block">Merchant<br class="d-none d-lg-block">Dengan Konsep Berbagi Ekonomi</p>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="cashback-center">
+            <div class="col-md-6 mb-5">
+              <div class="card px-5 px-md-3 py-lg-5">
+                <div class="row flex-center">
+                  <div class="bg-holder z-index-1 d-none d-lg-block" style="background-image:url(assets/img/illustrations/feature-bg-1.png);background-position:center;background-size:contain;">
+                  </div>
+
+                  <div class="bg-holder z-index-1 d-block d-lg-none" style="background-image:url(assets/img/illustrations/feature-bg-1.png);background-position:center;background-size:cover;">
+                  </div>
+
+                  <div class="col-md-4 pe-0 pe-md-0 text-center"><img class="img-fluid" src="assets/img/illustrations/cashback.jpg" alt="" /></div>
+                  <div class="col-md-8 ps-md-3 pe-md-2 text-center text-md-start z-index-2">
+                    <div class="card-body px-0">
+                      <h4 class="card-title pt-md-5">Cashback </h4>
+                      <p class="mb-0" style="font-size: 1em;"> Aplikasi Belanja Yang Memberikan <br class="d-none d-lg-block">Cashback Dari Semua Transaksi <br class="d-none d-lg-block">Antara Member Dan Merchant<br class="d-none d-lg-block">Merubah Pengeluaran <br class="d-none d-lg-block">Menjadi Penghasilan</p>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            
+        </div>
+      </section>
+      <section class="py-5">
+        <div class="container-lg bg-info p-5 p-md-5 p-xl-7 rounded-3">
+          <div class="row flex-center">
+            <div class="col-12">
+              <h2 class="text-light fw-bold">Testimonials</h2>
+            </div>
+          </div>
+          <div class="carousel slide pt-6" id="carouselExampleDark" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active" data-bs-interval="10000">
+                <div class="row h-100">
+                  <div class="col-md-4 mb-3 mb-md-0">
+                    <div class="card h-100 py-3">
+                      <div class="card-body my-2">
+                        <div class="d-flex align-items-center"><img class="img-fluid me-3 me-md-2 me-lg-3" src="https://developers.google.com/search/images/google-search-central-logo.png" width="70" alt="" />
+                          <div class="flex-1 align-items-center">
+                            <h6 class="mb-0 fs--1 text-1000 fw-medium">Jonathan S </h6>
+                            <p class="fs--2 fw-normal text-info mb-0">jonathan.com</p>
+                          </div>
+                        </div>
+                        <p class="card-text ps-7 ps-md-0 ps-xl-7 pt-md-4 pt-lg-3 pt-xl-0">“kalao ngantuk ya tidur cuy”.</p>
+                      </div>
+                    </div>
+                  </div>
+                  
+            <div class="row px-3 px-md-0 mt-4">
+              <div class="col-6 position-relative">
+                <ol class="carousel-indicators">
+                  <li class="active" data-bs-target="#carouselExampleDark" data-bs-slide-to="0"></li>
+                  <li data-bs-target="#carouselExampleDark" data-bs-slide-to="1"></li>
+                  <li data-bs-target="#carouselExampleDark" data-bs-slide-to="2"></li>
+                </ol>
+              </div>
+              <div class="col-6 position-relative"><a class="carousel-control-prev carousel-icon z-index-2" href="#carouselExampleDark" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next carousel-icon z-index-2" href="#carouselExampleDark" role="button" data-bs-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+     
+      <section class="py-6 pb-0">
+
+        <div class="container">
+          <hr class="text-info opacity-25" />
+          <div class="row py-7 justify-content-sm-between text-center text-md-start">
+          <div class="row justify-content-lg-around">
+            <div class="col-12 col-sm-12 col-lg-3 mb-4 order-0 order-sm-0"><a class="text-decoration-none" href="#"><img class="img-fluid me-3" src="https://www.vave.co.id/wp-content/uploads/2020/10/cropped-V-icon-192x192.png" alt="" width="50px"/><span class="fw-bold fs-1 text-1000">VAVE GLOBAL</span></a>
+              <p class="mt-4">Griya Bakoel Rollas <br/>Jl. Taman Bintaro Raya No 8 <br/>Jakarta Selatan - Indonesia</p>
+            </div>
+            <div class="col-6 col-sm-4 col-lg-3 mb-3 order-2 order-sm-1">
+              <h6 class="lh-lg fw-bold mb-4">Home</h6>
+              <ul class="list-unstyled mb-md-4 mb-lg-0">
+                <li class="lh-lg"><a class="text-dark fs--1 text-decoration-none" href="#!">Tentang Kami</a></li>
+                <li class="lh-lg"><a class="text-dark fs--1 text-decoration-none" href="#!">Layanan Kami</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-sm-4 col-lg-3 mb-3 order-3 order-sm-2">
+              <h6 class="lh-lg fw-bold mb-4"> Help </h6>
+              <ul class="list-unstyled mb-md-4 mb-lg-0">
+                <li class="lh-lg"><a class="text-dark fs--1 text-decoration-none" href="#!">Pusat Bantuan</a></li>
+               
+              </ul>
+              
+            </div>
+            <div class="col-12 col-sm-4 col-lg-3 mb-3 order-1 order-sm-3">
+              <h6 class="lh-lg fw-bold mb-4">Unduh </h6>
+              <ul class="list-unstyled mb-md-4 mb-lg-0">
+                <li class="list-inline-item"><a class="text-dark fs--1 text-decoration-none" href="#!"><img class="img-fluid" src="assets/img/icons/banner-appstore.png" width="100" alt="" /></a></li>
+                <li class="list-inline-item"><a class="text-dark fs--1 text-decoration-none" href="#!"><img class="img-fluid" src="assets/img/icons/banner-playstore.png" width="100" alt="" /></a></li>
+              </ul>
+              <br/>
+              <h6 class="lh-lg fw-bold mb-4">Social Media </h6>
+              <ul class="list-unstyled mb-md-4 mb-lg-0">
+                <li class="list-inline-item"><a class="text-dark fs--1 text-decoration-none" href="https://www.facebook.com/Vave-Brite-104820194843410/"><img class="img-fluid" src="assets/img/icons/f-icon.png" width="40" alt="" /></a></li>
+                <li class="list-inline-item"><a class="text-dark fs--1 text-decoration-none" href="https://youtube.com/c/VAVEBRITEOfficial"><img class="img-fluid" src="assets/img/icons/y-icon.png" width="40" alt="" /></a></li>
+                <li class="list-inline-item"><a class="text-dark fs--1 text-decoration-none" href="https://www.instagram.com/vavebriteofficial?r=nametag"><img class="img-fluid" src="assets/img/icons/i-icon.png" width="40" alt="" /></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+      <div class="footer">
+			<p style="font-size: 1.75vh; color: black;">© 2021 - Vaveglobal | Adalah Merek Milik PT Vave Global Madani</p>
+		  </div>
+  
+
+
+
+      
+    
+
+    </main>
+    <!-- end of source -->
+    <button id="back2top"><img src="assets/img/arrow-up.png"></img></button>
+    <script src="assets/js/back2top.min.js"></script>
+    <script>
+    const button = document.querySelector("#back2top");
+    BACK2TOP(button, 50);
+    </script>	
+
+    <script src="vendors/@popperjs/popper.min.js"></script>
+    <script src="vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="vendors/is/is.min.js"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="assets/js/theme.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,700;1,900&amp;display=swap" rel="stylesheet">
+  </body>
+
+</html>
