273490b4cb620eb507c194030f167d985705d986
syntaxjo45 <jonathansianturi48@gmail.com>
2021-03-12 18:10:49 +0700

    New Push

---
+++ assets/js/mapdata.js
@@ -0,0 +1,194 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+		width: "responsive", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    pop_ups: "detect",
+    
+		//State defaults
+		state_description: "Deskripsi ex: (Brp Pengguna dll)",
+    state_color: "#8b8c8f",
+    state_hover_color: "#328073",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_url: "https://www.google.com/maps/dir//''/@-6.27273,106.7230816,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x2e69f04ff673b409:0xd77be298db89e5c7!2m2!1d106.7581013!2d-6.2728165",
+    location_color: "#328073",
+    location_size: 30,
+    location_image_source: "http://facebook.com/favicon.ico",
+    location_type: "star",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    IDN1136: {
+      name: "Aceh",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    IDN1185: {
+      name: "Kalimantan Timur",
+      color: "#68C9BA"
+    },
+    IDN1223: {
+      name: "Jawa Barat",
+      color: "#68C9BA"
+    },
+    IDN1224: {
+      name: "Jawa Tengah",
+      color: "#68C9BA"
+    },
+    IDN1225: {
+      name: "Bengkulu"
+    },
+    IDN1226: {
+      name: "Banten"
+    },
+    IDN1227: {
+      name: "Jakarta Raya",
+      description: "default"
+    },
+    IDN1228: {
+      name: "Kalimantan Barat",
+      color: "#68C9BA"
+    },
+    IDN1229: {
+      name: "Lampung",
+      color: "#68C9BA"
+    },
+    IDN1230: {
+      name: "Sumatera Selatan"
+    },
+    IDN1231: {
+      name: "Bangka-Belitung"
+    },
+    IDN1232: {
+      name: "Bali",
+      color: "#68C9BA"
+    },
+    IDN1233: {
+      name: "Jawa Timur",
+      color: "#68C9BA"
+    },
+    IDN1234: {
+      name: "Kalimantan Selatan",
+      color: "#68C9BA"
+    },
+    IDN1235: {
+      name: "Nusa Tenggara Timur"
+    },
+    IDN1236: {
+      name: "Sulawesi Selatan"
+    },
+    IDN1237: {
+      name: "Sulawesi Barat"
+    },
+    IDN1796: {
+      name: "Kepulauan Riau"
+    },
+    IDN1837: {
+      name: "Gorontalo"
+    },
+    IDN1930: {
+      name: "Jambi"
+    },
+    IDN1931: {
+      name: "Kalimantan Tengah",
+      color: "#68C9BA"
+    },
+    IDN1933: {
+      name: "Irian Jaya Barat"
+    },
+    IDN381: {
+      name: "Sumatera Utara"
+    },
+    IDN492: {
+      name: "Riau"
+    },
+    IDN513: {
+      name: "Sulawesi Utara"
+    },
+    IDN538: {
+      name: "Maluku Utara"
+    },
+    IDN539: {
+      name: "Sumatera Barat"
+    },
+    IDN540: {
+      name: "Yogyakarta"
+    },
+    IDN554: {
+      name: "Maluku"
+    },
+    IDN555: {
+      name: "Nusa Tenggara Barat"
+    },
+    IDN556: {
+      name: "Sulawesi Tenggara"
+    },
+    IDN557: {
+      name: "Sulawesi Tengah"
+    },
+    IDN558: {
+      name: "Papua",
+      color: "#68C9BA"
+    }
+  },
+  locations: {
+    "0": {
+      location_url: "default",
+      lat: "-6.2725552126623745",
+      lng: "106.75814421252404",
+      name: "Vave Space",
+      description: "Direct To Vave Space? "
+    }
+  }
+};
